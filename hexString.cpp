c0ed2b7c17070e37f61c413016eefb6c4fa2ea2e
Christopher Rodney <ctrodney@gmail.com>
2015-04-30 01:52:26 -0700

    Lots of fixes to class.  Still cannot assign result of + and - operators, but += and -= (and all other operators) work fine.  Main functions getBits(int), getBits(int,int), toint() (all return int values.)

--- hexString.cpp
+++ hexString.cpp
@@ -8,7 +8,7 @@ hexString::hexString(){
 	intValue = -1;
 }
 
-hexString::hexString(std::string newHexString){
+hexString::hexString(const std::string newHexString){
 	std::string temp = "";
 	
 	//strip out any illegal characters and whitespace
@@ -18,16 +18,21 @@ hexString::hexString(std::string newHexString){
 		}
 		//Convert lowercase characters to uppercase  ASCII a-f
 		if (newHexString[j] > 96 && newHexString[j] < 103){
-			newHexString[j] -= 32;
-			temp += newHexString[j];
+			char tempchar = newHexString[j] - 32;
+			temp += tempchar;
 		}
 	}
 		
 	hexValue = temp;
-	intValue = -1;
+	setIntValue();
 }
 
-hexString::hexString(const char * newHexChar){
+hexString::hexString(const hexString& newHexString){
+	hexValue = newHexString.hexValue;
+	intValue = newHexString.intValue;
+}
+
+hexString::hexString(const char newHexChar[]){
 	std::string temp = "";
 	std::string newHexString(newHexChar);
 	
@@ -44,7 +49,7 @@ hexString::hexString(const char * newHexChar){
 	}
 		
 	hexValue = temp;
-	intValue = -1;
+	setIntValue();
 }
 
 bool hexString::isLegalChar(char testChar){
@@ -100,7 +105,6 @@ int hexString::getBits(int Bit1, int Bit2){
 }
 
 unsigned long long int hexString::toInt(){
-	if (intValue == -1){setIntValue();}
 	return intValue;
 }
 
@@ -140,111 +144,115 @@ void hexString::setHexValue(){
 	hexValue = intToHex(tempInt) + hexValue;	
 } 
 
-hexString& hexString::operator = (hexString Right){
+void hexString::operator = (const hexString& Right){
 	hexValue = Right.hexValue;
-	return *this;
+	intValue = Right.intValue;
 }
 
-hexString& hexString::operator = (unsigned long long int  Right){
+void hexString::operator = (const unsigned long long int  Right){
 	intValue = Right;
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString::operator = (std::string &Right){
-	hexValue = Right;
-	return *this;
+void hexString::operator = (const std::string Right){
+	std::string temp = "";
+	
+	//strip out any illegal characters and whitespace
+	for (int j = 0; j < Right.length(); j++){
+		if (isLegalChar(Right[j])) {
+			temp += Right[j];
+		}
+		//Convert lowercase characters to uppercase  ASCII a-f
+		if (Right[j] > 96 && Right[j] < 103){
+			char tempchar = Right[j] - 32;
+			temp += tempchar;
+		}
+	}
+		
+	hexValue = temp;
+	setIntValue();;
 }
 
-hexString& hexString::operator = (const char *Right){
-	hexValue = std::string(Right);
-	return *this;
+void hexString::operator = (const char Right[]){
+	std::string temp = "";
+	std::string newHexString(Right);
+	
+	//strip out any illegal characters and whitespace
+	for (int j = 0; j < newHexString.length(); j++){
+		if (isLegalChar(newHexString[j])) {
+			temp += newHexString[j];
+		}
+		//Convert lowercase characters to uppercase  ASCII a-f
+		if (newHexString[j] > 96 && newHexString[j] < 103){
+			newHexString[j] -= 32;
+			temp += newHexString[j];
+		}
+	}
+		
+	hexValue = temp;
+	setIntValue();
 }
 
-hexString& hexString:: operator += (hexString &Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator += (hexString Right){
 	intValue += Right.toInt();
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString:: operator += (unsigned long long int Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator += (const unsigned long long int Right){
 	intValue += Right;
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString:: operator += (std::string &Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator += (const std::string Right){
 	intValue += hexString(Right).toInt();
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString:: operator += (char &Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator += (const char Right[]){
 	intValue += hexString(Right).toInt();
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString:: operator -= (hexString &Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator -= (hexString Right){
 	intValue -= Right.toInt();
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString:: operator -= (unsigned long long int Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator -= (const unsigned long long int Right){
 	intValue -= Right;
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString:: operator -= (std::string &Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator -= (const std::string Right){
 	intValue -= hexString(Right).toInt();
 	setHexValue();
-	return *this;
 }
 
-hexString& hexString:: operator -= (char &Right){
-	if (intValue == -1){setIntValue();}
+void hexString::operator -= (const char Right[]){
 	intValue -= hexString(Right).toInt();
 	setHexValue();
-	return *this;
 }
 
-hexString hexString:: operator ++ (int){
+void hexString::operator ++ (int){
 	hexString temp(hexValue);	
-	if (intValue == -1){setIntValue();}
 	intValue++;
 	setHexValue();
-	return temp;
 }
 
-hexString& hexString:: operator ++ (){
-	if (intValue == -1){setIntValue();}
+void hexString::operator ++ (){
 	intValue++;
 	setHexValue();
-	return *this;
 }
 
-hexString hexString:: operator -- (int){
+void hexString::operator -- (int){
 	hexString temp(hexValue);
-	if (intValue == -1){setIntValue();}
 	intValue--;
 	setHexValue();
-	return temp;
 }
 
-hexString& hexString:: operator -- (){
-	if (intValue == -1){setIntValue();}
+void hexString::operator -- (){
 	intValue--;
 	setHexValue();
-	return *this;
 }
 
 
@@ -253,72 +261,140 @@ std::ostream& operator<<(std::ostream &left, hexString &right){
 	return left;
 }
 
-bool operator < (hexString &Left, hexString &Right){return (Left.toInt() < Right.toInt());}
-bool operator < (hexString &Left, unsigned long long int Right){return (Left.toInt() < Right);}
-bool operator < (hexString &Left, std::string &Right){return (Left.toInt() < hexString(Right).toInt());}
-bool operator < (unsigned long long int Left, hexString &Right){return (Left < Right.toInt());}
-bool operator < (std::string &Left, hexString &Right){return (hexString(Left).toInt() < Right.toInt());}
-bool operator < (hexString &Left, char &Right){return (Left.toInt() < hexString(Right).toInt());}
-bool operator < (char &Left, hexString &Right){return (hexString(Left).toInt() < Right.toInt());}
+bool operator < ( hexString Left,  hexString Right){return (Left.toInt() < Right.toInt());}
+bool operator < ( hexString Left,  unsigned long long int Right){return (Left.toInt() < Right);}
+bool operator < ( hexString Left,  std::string Right){return (Left.toInt() < hexString(Right).toInt());}
+bool operator < ( unsigned long long int Left,  hexString Right){return (Left < Right.toInt());}
+bool operator < ( std::string Left,  hexString Right){return (hexString(Left).toInt() < Right.toInt());}
+bool operator < ( hexString Left,  const char Right[]){return (Left.toInt() < hexString(Right).toInt());}
+bool operator < ( const char Left[],  hexString Right){return (hexString(Left).toInt() < Right.toInt());}
 
-bool operator <= (hexString &Left, hexString &Right){return (Left.toInt() <= Right.toInt());}
-bool operator <= (hexString &Left, unsigned long long int Right){return (Left.toInt() <= Right);}
-bool operator <= (hexString &Left, std::string &Right){return (Left.toInt() <= hexString(Right).toInt());}
-bool operator <= (unsigned long long int Left, hexString &Right){return (Left <= Right.toInt());}
-bool operator <= (std::string &Left, hexString &Right){return (hexString(Left).toInt() <= Right.toInt());}
-bool operator <= (hexString &Left, char &Right){return (Left.toInt() <= hexString(Right).toInt());}
-bool operator <= (char &Left, hexString &Right){return (hexString(Left).toInt() <= Right.toInt());}
-
-
-bool operator > (hexString &Left, hexString &Right){return (Left.toInt() > Right.toInt());}
-bool operator > (hexString &Left, unsigned long long int Right){return (Left.toInt() > Right);}
-bool operator > (hexString &Left, std::string &Right){	return (Left.toInt() > hexString(Right).toInt());}
-bool operator > (unsigned long long int Left, hexString &Right){return (Left > Right.toInt());}
-bool operator > (std::string &Left, hexString &Right){return (hexString(Left).toInt() > Right.toInt());}	
-bool operator > (hexString &Left, char &Right){return (Left.toInt() > hexString(Right).toInt());}
-bool operator > (char &Left, hexString &Right){return (hexString(Left).toInt() > Right.toInt());}
+bool operator <= ( hexString Left,  hexString Right){return (Left.toInt() <= Right.toInt());}
+bool operator <= ( hexString Left,  unsigned long long int Right){return (Left.toInt() <= Right);}
+bool operator <= ( hexString Left,  std::string Right){return (Left.toInt() <= hexString(Right).toInt());}
+bool operator <= ( unsigned long long int Left,  hexString Right){return (Left <= Right.toInt());}
+bool operator <= ( std::string Left,  hexString Right){return (hexString(Left).toInt() <= Right.toInt());}
+bool operator <= ( hexString Left,  const char Right[]){return (Left.toInt() <= hexString(Right).toInt());}
+bool operator <= ( const char Left[],  hexString Right){return (hexString(Left).toInt() <= Right.toInt());}
 
 
-bool operator >= (hexString &Left, hexString &Right){return (Left.toInt() >= Right.toInt());}
-bool operator >= (hexString &Left, unsigned long long int Right){return (Left.toInt() >= Right);}
-bool operator >= (hexString &Left, std::string &Right){return (Left.toInt() >= hexString(Right).toInt());}
-bool operator >= (unsigned long long int Left, hexString &Right){return (Left >= Right.toInt());}
-bool operator >= (std::string &Left, hexString &Right){return (hexString(Left).toInt() >= Right.toInt());}
-bool operator >= (hexString &Left, char &Right){return (Left.toInt() >= hexString(Right).toInt());}
-bool operator >= (char &Left, hexString &Right){return (hexString(Left).toInt() >= Right.toInt());}
+bool operator > ( hexString Left,  hexString Right){return (Left.toInt() > Right.toInt());}
+bool operator > ( hexString Left,  unsigned long long int Right){return (Left.toInt() > Right);}
+bool operator > ( hexString Left,  std::string Right){	return (Left.toInt() > hexString(Right).toInt());}
+bool operator > ( unsigned long long int Left,  hexString Right){return (Left > Right.toInt());}
+bool operator > ( std::string Left,  hexString Right){return (hexString(Left).toInt() > Right.toInt());}	
+bool operator > ( hexString Left,  const char Right[]){return (Left.toInt() > hexString(Right).toInt());}
+bool operator > ( const char Left[],  hexString Right){return (hexString(Left).toInt() > Right.toInt());}
 
-		
-bool operator == (hexString &Left, hexString &Right){return (Left.hexValue == Right.hexValue);}
-bool operator == (hexString &Left, unsigned long long int Right){return (Left.toInt() == Right);}
-bool operator == (hexString &Left, std::string &Right){return (Left.hexValue == Right);}
-bool operator == (unsigned long long int Left, hexString &Right){return (Left == Right.toInt());}
-bool operator == (std::string &Left, hexString &Right){return (Left == Right.hexValue);}
-bool operator == (hexString &Left, char &Right){return (Left.toInt() == hexString(Right).toInt());}
-bool operator == (char &Left, hexString &Right){return (hexString(Left).toInt() == Right.toInt());}
+
+bool operator >= ( hexString Left,  hexString Right){return (Left.toInt() >= Right.toInt());}
+bool operator >= ( hexString Left,  unsigned long long int Right){return (Left.toInt() >= Right);}
+bool operator >= ( hexString Left,  std::string Right){return (Left.toInt() >= hexString(Right).toInt());}
+bool operator >= ( unsigned long long int Left,  hexString Right){return (Left >= Right.toInt());}
+bool operator >= ( std::string Left,  hexString Right){return (hexString(Left).toInt() >= Right.toInt());}
+bool operator >= ( hexString Left,  const char Right[]){return (Left.toInt() >= hexString(Right).toInt());}
+bool operator >= ( const char Left[],  hexString Right){return (hexString(Left).toInt() >= Right.toInt());}
 
 		
-bool operator != (hexString &Left, hexString &Right){return (Left.hexValue != Right.hexValue);}
-bool operator != (hexString &Left, unsigned long long int Right){return (Left.toInt() != Right);}
-bool operator != (hexString &Left, std::string &Right){return (Left.hexValue != Right);}
-bool operator != (unsigned long long int Left, hexString &Right){return (Left != Right.toInt());}
-bool operator != (std::string &Left, hexString &Right){return (Left != Right.hexValue);}
-bool operator != (hexString &Left, char &Right){return (Left.toInt() != hexString(Right).toInt());}
-bool operator != (char &Left, hexString &Right){return (hexString(Left).toInt() != Right.toInt());}
+bool operator == ( hexString Left,  hexString Right){return (Left.hexValue == Right.hexValue);}
+bool operator == ( hexString Left,  unsigned long long int Right){return (Left.toInt() == Right);}
+bool operator == ( hexString Left,  std::string Right){return (Left.hexValue == Right);}
+bool operator == ( unsigned long long int Left,  hexString Right){return (Left == Right.toInt());}
+bool operator == ( std::string Left,  hexString Right){return (Left == Right.hexValue);}
+bool operator == ( hexString Left,  const char Right[]){return (Left.toInt() == hexString(Right).toInt());}
+bool operator == ( const char Left[],  hexString Right){return (hexString(Left).toInt() == Right.toInt());}
+
+		
+bool operator != ( hexString Left,  hexString Right){return (Left.hexValue != Right.hexValue);}
+bool operator != ( hexString Left,  unsigned long long int Right){return (Left.toInt() != Right);}
+bool operator != ( hexString Left,  std::string Right){return (Left.hexValue != Right);}
+bool operator != ( unsigned long long int Left,  hexString Right){return (Left != Right.toInt());}
+bool operator != ( std::string Left,  hexString Right){return (Left != Right.hexValue);}
+bool operator != ( hexString Left,  const char Right[]){return (Left.toInt() != hexString(Right).toInt());}
+bool operator != ( const char Left[],  hexString Right){return (hexString(Left).toInt() != Right.toInt());}
 
 
-hexString operator + (hexString Left, hexString Right){return (hexString(Left.toInt() + Right.intValue));}
-hexString operator + (hexString Left, unsigned long long int Right){return (hexString(Left.toInt() + Right));}
-hexString operator + (hexString Left, std::string &Right){return(hexString(Left.toInt() + hexString(Right).toInt()));}
-hexString operator + (unsigned long long int Left, hexString Right){return (hexString(Left + Right.toInt()));}
-hexString operator + (std::string &Left, hexString Right){return(hexString(hexString(Left).toInt() + Right.toInt()));}
-hexString operator + (hexString Left, char &Right){return(hexString(Left.toInt() + hexString(Right).toInt()));}
-hexString operator + (char &Left, hexString Right){return(hexString(hexString(Left).toInt() + Right.toInt()));}
+hexString operator + (const hexString Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() + Right.intValue;
+	return RetVal;
+}
 
-hexString operator - (hexString Left, hexString Right){return (hexString(Left.toInt() - Right.intValue));}
-hexString operator - (hexString Left, unsigned long long int Right){return (hexString(Left.toInt() - Right));}
-hexString operator - (hexString Left, std::string &Right){return(hexString(Left.toInt() - hexString(Right).toInt()));}
-hexString operator - (unsigned long long int Left, hexString Right){return(hexString(Left - Right.toInt()));}
-hexString operator - (std::string &Left, hexString Right){return(hexString(hexString(Left).toInt() - Right.toInt()));}
-hexString operator - (hexString Left, char &Right){return(hexString(Left.toInt() - hexString(Right).toInt()));}
-hexString operator - (char &Left, hexString Right){return(hexString(hexString(Left).toInt() - Right.toInt()));}
+hexString operator + (const hexString Left, const unsigned long long int Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() + Right;
+	return RetVal;
+}
+
+hexString operator + (const hexString Left, const std::string Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() + hexString(Right).toInt();
+	return RetVal;
+}
+
+hexString operator + (const unsigned long long int Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = Left + Right.intValue;
+	return RetVal;
+}
+
+hexString operator + (const std::string Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() + Right.intValue;
+	return RetVal;
+}
+
+hexString operator + (const hexString Left,  const char * Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() + hexString(Right).toInt();
+	return RetVal;
+}
+
+hexString operator + (const char * Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() + Right.intValue;
+	return RetVal;
+}
+
+hexString operator - (const hexString Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() - Right.intValue;
+	return RetVal;
+}
+
+hexString operator - (const hexString Left, const unsigned long long int Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() - Right;
+	return RetVal;
+}
+
+hexString operator - (const hexString Left, const std::string Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() - hexString(Right).toInt();
+	return RetVal;
+}
+
+hexString operator - (const unsigned long long int Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = Left - Right.intValue;
+	return RetVal;
+}
+
+hexString operator - (const std::string Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() - Right.intValue;
+	return RetVal;
+}
+
+hexString operator - (const hexString Left,  const char * Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() - hexString(Right).toInt();
+	return RetVal;
+}
+
+hexString operator - (const char * Left, const hexString Right){
+	hexString RetVal;
+	RetVal.intValue = hexString(Left).toInt() - Right.intValue;
+	return RetVal;
+}
 
--- hexString.h
+++ hexString.h
@@ -7,99 +7,100 @@
 
 class hexString{
  		friend std::ostream& operator<<(std::ostream &Left, hexString &Right);
-		friend bool operator < (hexString &Left, hexString &Right);
-		friend bool operator < (hexString &Left, unsigned long long int Right);
-		friend bool operator < (hexString &Left, std::string &Right);
-		friend bool operator < (unsigned long long int Left, hexString &Right);
-		friend bool operator < (std::string &Left, hexString &Right);
-		friend bool operator < (hexString &Left, char &Right);
-		friend bool operator < (char &Left, hexString &Right);
+		friend bool operator < ( hexString Left,  hexString Right);
+		friend bool operator < ( hexString Left,  unsigned long long int Right);
+		friend bool operator < ( hexString Left,  std::string Right);
+		friend bool operator < ( unsigned long long int Left,  hexString Right);
+		friend bool operator < ( std::string Left,  hexString Right);
+		friend bool operator < ( hexString Left,  const char Right[]);
+		friend bool operator < ( const char Left[],  hexString Right);
 		
-		friend bool operator <= (hexString &Left, hexString &Right);
-		friend bool operator <= (hexString &Left, unsigned long long int Right);
-		friend bool operator <= (hexString &Left, std::string &Right);
-		friend bool operator <= (unsigned long long int Left, hexString &Right);
-		friend bool operator <= (std::string &Left, hexString &Right);
-		friend bool operator <= (hexString &Left, char &Right);
-		friend bool operator <= (char &Left, hexString &Right);
+		friend bool operator <= ( hexString Left,  hexString Right);
+		friend bool operator <= ( hexString Left,  unsigned long long int Right);
+		friend bool operator <= ( hexString Left,  std::string Right);
+		friend bool operator <= ( unsigned long long int Left,  hexString Right);
+		friend bool operator <= ( std::string Left,  hexString Right);
+		friend bool operator <= ( hexString Left,  const char Right[]);
+		friend bool operator <= ( const char Left[],  hexString Right);
 		
-		friend bool operator > (hexString &Left, hexString &Right);
-		friend bool operator > (hexString &Left, unsigned long long int Right);
-		friend bool operator > (hexString &Left, std::string &Right);
-		friend bool operator > (unsigned long long int Left, hexString &Right);
-		friend bool operator > (std::string &Left, hexString &Right);
-		friend bool operator > (hexString &Left, char &Right);
-		friend bool operator > (char &Left, hexString &Right);
+		friend bool operator > ( hexString Left,  hexString Right);
+		friend bool operator > ( hexString Left,  unsigned long long int Right);
+		friend bool operator > ( hexString Left,  std::string Right);
+		friend bool operator > ( unsigned long long int Left,  hexString Right);
+		friend bool operator > ( std::string Left,  hexString Right);
+		friend bool operator > ( hexString Left,  const char Right[]);
+		friend bool operator > ( const char Left[],  hexString Right);
 
-		friend bool operator >= (hexString &Left, hexString &Right);
-		friend bool operator >= (hexString &Left, unsigned long long int Right);
-		friend bool operator >= (hexString &Left, std::string &Right);
-		friend bool operator >= (unsigned long long int Left, hexString &Right);
-		friend bool operator >= (std::string &Left, hexString &Right);
-		friend bool operator >= (hexString &Left, char &Right);
-		friend bool operator >= (char &Left, hexString &Right);
+		friend bool operator >= ( hexString Left,  hexString Right);
+		friend bool operator >= ( hexString Left,  unsigned long long int Right);
+		friend bool operator >= ( hexString Left,  std::string Right);
+		friend bool operator >= ( unsigned long long int Left,  hexString Right);
+		friend bool operator >= ( std::string Left,  hexString Right);
+		friend bool operator >= ( hexString Left,  const char Right[]);
+		friend bool operator >= ( const char Left[],  hexString Right);
 		
-		friend bool operator == (hexString &Left, hexString &Right);
-		friend bool operator == (hexString &Left, unsigned long long int Right);
-		friend bool operator == (hexString &Left, std::string &Right);
-		friend bool operator == (unsigned long long int Left, hexString &Right);
-		friend bool operator == (std::string &Left, hexString &Right);
-		friend bool operator == (hexString &Left, char &Right);
-		friend bool operator == (char &Left, hexString &Right);
+		friend bool operator == ( hexString Left,  hexString Right);
+		friend bool operator == ( hexString Left,  unsigned long long int Right);
+		friend bool operator == ( hexString Left,  std::string Right);
+		friend bool operator == ( unsigned long long int Left,  hexString Right);
+		friend bool operator == ( std::string Left,  hexString Right);
+		friend bool operator == ( hexString Left,  const char Right[]);
+		friend bool operator == ( const char Left[],  hexString Right);
 		
-		friend bool operator != (hexString &Left, hexString &Right);
-		friend bool operator != (hexString &Left, unsigned long long int Right);
-		friend bool operator != (hexString &Left, std::string &Right);
-		friend bool operator != (unsigned long long int Left, hexString &Right);
-		friend bool operator != (std::string &Left, hexString &Right);
-		friend bool operator != (hexString &Left, char &Right);
-		friend bool operator != (char &Left, hexString &Right);
+		friend bool operator != ( hexString Left,  hexString Right);
+		friend bool operator != ( hexString Left,  unsigned long long int Right);
+		friend bool operator != ( hexString Left,  std::string Right);
+		friend bool operator != ( unsigned long long int Left,  hexString Right);
+		friend bool operator != ( std::string Left,  hexString Right);
+		friend bool operator != ( hexString Left,  const char Right[]);
+		friend bool operator != ( const char Left[],  hexString Right);
 		
-		friend hexString operator + (hexString Left, hexString Right);
-		friend hexString operator + (hexString Left, unsigned long long int Right);
-		friend hexString operator + (hexString Left, std::string &Right);
-		friend hexString operator + (unsigned long long int Left, hexString Right);
-		friend hexString operator + (std::string &Left, hexString Right);
-		friend hexString operator + (hexString Left, char &Right);
-		friend hexString operator + (char &Left, hexString Right);
+		friend hexString operator + (const hexString Left, const hexString Right);
+		friend hexString operator + (const hexString Left, const unsigned long long int Right);
+		friend hexString operator + (const hexString Left, const std::string Right);
+		friend hexString operator + (const unsigned long long int Left, const hexString Right);
+		friend hexString operator + (const std::string Left, const hexString Right);
+		friend hexString operator + (const hexString Left, const char * Right);
+		friend hexString operator + (const char * Left, const hexString Right);
 		
-		friend hexString operator - (hexString Left, hexString Right);
-		friend hexString operator - (hexString Left, unsigned long long int Right);
-		friend hexString operator - (hexString Left, std::string &Right);
-		friend hexString operator - (unsigned long long int Left, hexString Right);
-		friend hexString operator - (std::string &Left, hexString Right);
-		friend hexString operator - (hexString Left, char &Right);
-		friend hexString operator - (char &Left, hexString Right);
+		friend hexString operator - (const hexString Left, const hexString Right);
+		friend hexString operator - (const hexString Left, const unsigned long long int Right);
+		friend hexString operator - (const hexString Left, const std::string Right);
+		friend hexString operator - (const unsigned long long int Left, const hexString Right);
+		friend hexString operator - (const std::string Left, const hexString Right);
+		friend hexString operator - (const hexString Left, const char * Right);
+		friend hexString operator - (const char * Left, const hexString Right);
 	public:
 		hexString();
-		hexString(std::string newHexString);
-		hexString(const char * newHexString);
+		hexString(const std::string newHexString);
+		hexString(const hexString& newHexString);
+		hexString(const char newHexString[] );
 		hexString(unsigned long long int newHexString);
 		~hexString();
 		int getBits(int Bit);
 		int getBits(int Bit1, int Bit2);
 		unsigned long long int toInt();
 		
-		hexString& operator = (hexString Right);
-		hexString& operator = (unsigned long long int Right);
-		hexString& operator = (std::string &Right);
-		hexString& operator = (const char *Right);
+		void operator = (const hexString& Right);
+		void operator = (const unsigned long long int Right);
+		void operator = (const std::string Right);
+		void operator = (const char Right[]);
 		
-		hexString& operator += (hexString &Right);
-		hexString& operator += (unsigned long long int Right);
-		hexString& operator += (std::string &Right);
-		hexString& operator += (char &Right);
+		void operator += (hexString Right);
+		void operator += (const unsigned long long int Right);
+		void operator += (const std::string Right);
+		void operator += (const char Right[]);
 		
-		hexString& operator -= (hexString &Right);
-		hexString& operator -= (unsigned long long int Right);
-		hexString& operator -= (std::string &Right);
-		hexString& operator -= (char &Right);
+		void operator -= (hexString Right);
+		void operator -= (const unsigned long long int Right);
+		void operator -= (const std::string Right);
+		void operator -= (const char Right[]);
 		
-		hexString operator ++ (int) ;
-		hexString& operator ++ ();
+		void operator ++ (int) ;
+		void operator ++ ();
 		
-		hexString operator -- (int);
-		hexString& operator -- ();
+		void operator -- (int);
+		void operator -- ();
 	private:
 		std::string hexValue;
 		unsigned long long int intValue;
