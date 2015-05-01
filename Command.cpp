4d7ddd6997c4fa8cca08200471df03028f9b6b15
Christopher Rodney <ctrodney@gmail.com>
2015-04-30 20:18:04 -0700

    Updated Command Class with printing and constructor functions.

--- Command.cpp
+++ Command.cpp
@@ -3,15 +3,85 @@
 #include <vector>
 #include "Command.h"
 
-Command::Command()
-{
-	Data = {};	// populate Data vector
+Command::Command(int Line, WhichCommand Type, hexString LineCount){
+	SDBaseAddress = "40000818";
+	DSBaseAddress = "40000C20";
+	string ThisLine;
+	ThisLine = "Line " + Line + ": ";
+	if (Tpye == WhichCommand.SDWrite) {ThisLine += "Write S-to-D command: ";}
+	if (Tpye == WhichCommand.SDRead) {ThisLine += "Read S-to-D command: ";}
+	if (Tpye == WhichCommand.DSWrite) {ThisLine += "Write D-to-S command: ";}
+	if (Tpye == WhichCommand.DSRead) {ThisLine += "Write D-to-S command: ";}
+	ThisLine += std::to_string(LineCount.toInt());
+	ThisLine += " words";
+	CommandsProcessed.push_back(ThisLine);	
+	
 }
 
 Command::~Command() {}
 
+void Command::AddData(int Line, string CommandData, hexString Address){
+	hexString Position;
+	Position = Address;
+	if ((Type == WhichCommand.SDRead) || (Type == WhichCommand.SDWrite)) {
+		Position -= SDBaseAddress;
+	} else {
+		Position -= DSBaseAddress;
+	}
+	int Position1 = Position.toInt();
+	Position1 /= 16;
+	int Position2 = Position1 + 1;
+	string Command1 = CommandData.substr(0,4);
+	string Command2 = CommandData.substr(4,4);
+	Data[Position1] = Command1;
+	Data[Position2] = Command2;
+	
+	AddPrintLine(Line, Position1);
+	AddPrintLine(Line, Position2);	
+}
 
+bool MoreCommands(){
+	return (PrintCounter >= CommandsProcessed.size());
+}
 
+void ResetPrintPosition(){PrintCounter = 0;}
+
+void AddPrintLine(int Line, int Position){
+	string ThisLine;
+	ThisLine = "Line " + Line + ": Word ";
+	ThisLine += std::to_string(Position);
+	ThisLine += ": ";
+	switch (Position){
+		case 0 : ThisLine += "Rec_Ctrl = " + getRec_Ctrl();
+		case 1 : ThisLine += "Cmd_Type = " + getCmd_Type();
+		case 4 : ThisLine += "Rec_Raw = " + getRec_Raw();
+		case 5 : ThisLine += "Cmd_ID = " + getCmd_ID();
+		case 10 : ThisLine += "Num_Responses = " + getNum_Responses();
+		case 15 : ThisLine += "Reset_Enable = " + getReset_Enable();
+		case 22 : ThisLine += "Direction = " + getDirection();
+		case 32 : ThisLine += "Num_Samples = " + getNum_Samples();
+		case 37 : ThisLine += "Parity = " + getParity();
+		case 38 : ThisLine += "Test = " + getTest();
+		case 40 : ThisLine += "Ctrl_Enable = " + getCtrl_Enable();
+		case 41 : ThisLine += "Code = " + getCode();
+	}
+	
+}
+
+std::string Command::FetchNextCommand(){
+	string RetVal;
+	if (PrintCounter < CommandsProcessed.size()){
+		RetVal << CommandsProcessed[PrintCounter];
+	}
+	PrintCounter++;
+	return RetVal;
+}
+
+std::string Command::PrintAllCommands(){
+	for (int i = 0; i < CommandsProcessed.size(); i++){
+		cout << CommandsProcessed[i] << endl;
+	}	
+}
 
 std::string Command::getRec_Ctrl()
 {
--- Command.h
+++ Command.h
@@ -4,13 +4,20 @@
 #include <vector>
 #include "hexString.h"
 
+enum WhichCommand {SDWrite, SDRead, DSWrite, DSRead};
+
 class Command
 {
 
 
 	public:
-		Command();
+		Command(int Line, WhichCommand Type, hexString LineCount);
 		~Command();
+		void AddData(int Line, std::string CommandData, hexString Address);
+		std::string FetchNextCommand();
+		std::string PrintAllCommands();
+		bool MoreCommands();
+		void ResetPrintPosition();
 		std::string getRec_Ctrl();
 		std::string getCmd_Type();
 		std::string getRec_Raw();
@@ -24,13 +31,15 @@ class Command
 		std::string getCtrl_Enable();
 		int getCode();
 
-
-
-
-
 	private:
 
-		std::vector<hexString> Data;	
+		hexString Data[45];
+		std::vector<std::string> CommandsProcessed;
+		int PrintCounter;
+		hexString SDBaseAddress;
+		hexString DSBaseAddress;
+		WhichCommand Type;
+		void AddPrintLine(int Line, int Position);
 
 
 
